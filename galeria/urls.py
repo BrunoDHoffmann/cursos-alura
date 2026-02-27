c3efa39ae624f5e37170ba1b7d75535449f33375
bergsonall <bruninhodubal@hotmail.com>
2026-02-27 14:57:16 -0300

    definindo names para urls para transicionar entre paginas

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
-from galeria.views import index
+from django.urls import path
+from galeria.views import index, imagem
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem'),
 ]
