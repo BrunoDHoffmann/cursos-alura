c1aa93c6c1bb02c1bed60d3a5fbb3b1938eea7d5
bergsonall <bruninhodubal@hotmail.com>
2026-02-28 21:19:07 -0300

    django admin/ createsuperuser/ modificando layout django-admin/ CRUD no painel admin

--- galeria/models.py
+++ galeria/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 class Fotografia(models.Model):
+    OPCOES_CATEGORIAS = [
+        ('NEBULOSA', 'Nebusola'),
+        ('ESTRELA', 'Estrela'),
+        ('GALAXIA', 'Galaxia'),
+        ('PLANETA', 'Planeta'),
+    ]
+    
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=100, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIAS, default='NEBULOSA')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
     
