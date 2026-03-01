6a64750b72db0a408618a2ddbfe764b8968651e0
bergsonall <bruninhodubal@hotmail.com>
2026-03-01 00:31:27 -0300

    adcionando data

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "ativo")
     list_display_links = ("nome", "id")
     search_fields = ("nome",)
+    list_per_page = 10
+    list_filter = ("categoria",)
+    list_editable = ("ativo",)
 
 # Register your models here.
 admin.site.register(Fotografia, ListandoFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Fotografia(models.Model):
     OPCOES_CATEGORIAS = [
@@ -13,6 +14,8 @@ class Fotografia(models.Model):
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIAS, default='NEBULOSA')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    ativo = models.BooleanField(default=False)
+    data_criacao = models.DateTimeField(default=datetime.now(), blank=False)
     
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
